ee187e7ec5bd641e5f7f09555b747e54c7188488
Eduardo Sanchez <eduardosanchezfoto@gmail.com>
2024-08-14 06:48:37 -0700

    controllers build out

---
+++ controllers/item.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express.Router()
+const Item = require('../models/Item')
+
+//Create Item
+router.post('/create', async (req,res) => {
+  try {
+ const {name, quantity} = req.body
+ const newItem = new Item({name, quantity})
+ await newItem.save()
+ res.status(201).json({new_item: newItem,})
+} catch(err) {
+    res.status(500).json({message: 'Failed to create item', error: err.message})
+}
+})
+
+//Get Item
+router.get('/get', async (req,res) => {
+    try {
+        const items = await Item.find()
+        res.status(200).json(items)
+    } catch (error) {
+        res.status(500).json({message: 'Failed to connect item', error: err.message})
+    }
+})
+
+//Update Item
+router.put('/update', async (req,res) => {
+    try {
+        const updatedItem = await findByIdAndUpdate(req.body.id, req.body, {new: true, runValidators: true})
+        if(!updatedItem) {
+            return res.status(404).json({message: "Item not found"})
+        }
+        res.status(404).json(updatedItem)
+    } catch (error) {
+        res.status(500).json({message: 'Failed to update item', error: err.message})
+    }
+})
+
+//Delete Item
+router.delete('/delete', async (req,res) => {
+    try {
+        const deletedItem = await Item.findByIdAndDelete(req.body.id)
+        if(!deletedItem) {
+            return res.status(404).json({message: 'Item not found'})
+        }
+        res.status(200).json({message: 'Item deleted'})
+    } catch (error) {
+        res.status(500).json({message: 'Failed to delete item', error: err.message})
+    }
+})
+
+module.exports = router
--- models/Item.js
+++ models/Item.js
@@ -11,4 +11,6 @@ const ItemSchema = new mongoose.Schema({
     }
 })
 
-module.exports = mongoose.model('Item',ItemSchema)
+module.exports = mongoose.model('Item', ItemSchema)
+
+
